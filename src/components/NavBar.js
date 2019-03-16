b88a6396236a50f528798ba9240e747a39811cb5
Becca Randall <becca.m.randall@gmail.com>
2019-03-16 11:17:37 -0500

    Added restart button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
 class NavBar extends Component {
   render() {
@@ -10,10 +11,23 @@ class NavBar extends Component {
                         <img src='/images/battleship-logo.png' alt="Battleship" width="112" height="28"/>
                     </a>
                 </div>
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-info is-inverted is-outlined" onClick={() => this.props.refresh()}>
+                                Restart
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </nav>
         </div>
     )
   }
 }
 
-export default NavBar
+const mapDispatchToProps = (dispatch) => ({
+    refresh: () => dispatch({type: 'REFRESH_BOARD', height: 10, width: 10})
+})
+
+export default connect(null, mapDispatchToProps)(NavBar)
